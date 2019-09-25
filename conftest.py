6b15d3a06386e18b447e08ac1fdb87dc87fe6b5e
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2019-09-25 12:26:37 +0200

    nie calkiem inteligentny login i logout ktory wszystko psuje

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ __author__ = 'mla'
 import pytest
 from fixture.application import Application
 
+fixture = None
 
 @pytest.fixture
 # @pytest.fixture(scope = "session") - how to fix it?
@@ -9,10 +10,19 @@ from fixture.application import Application
 # tests which don't pass with '(scope = "session")': test_mod_contact, test_modify_contact_firstname, test_modify_contact_middlename
 # why do they fail or why the others pass?
 def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
+    global fixture
+    if fixture is None:
+        fixture = Application()
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.ensure_login(username="admin", password="secret")
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture

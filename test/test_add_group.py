e17784a1d478536d568dd94835f69e7c0946d85a
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2019-09-27 14:31:40 +0200

    Dodane sprawdzenia do testow dot grup

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="qqqqqq", header="wwwww", footer="eeeeee"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
     #app.session.logout()
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)

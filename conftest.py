45b6f68854b3ef5697fe8a6b7f03ede625e27867
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2019-09-27 09:50:43 +0200

    Dodane sprawdzanie stanu sesji dot zadania 8

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,7 +3,8 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.group.modify2_first_group(Group(name="New group"))
+    #app.session.logout()
 
 def test_modify_group_header(app):
     app.group.modify2_first_group(Group(header="New header"))
-    app.session.logout()
+    #app.session.logout()

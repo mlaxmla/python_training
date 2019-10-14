60c933206d6e100790044e8b037c828b14d693aa
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2019-10-14 10:19:44 +0200

    dodany import danych testowych z json i dane testowe

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ __author__ = 'mla'
 import pytest
 import json
 import os.path
+import importlib
 from fixture.application import Application
 
 fixture = None
@@ -37,3 +38,12 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
     #parser.addoption("--username", action="store", default="admin")
     #parser.addoption("--password", action="store", default="secret")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
--- test/test_add_group_from_json.py
+++ test/test_add_group_from_json.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-from data.groups import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
-    group = Group(name="qqqqqq", header="wwwww", footer="eeeeee")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()

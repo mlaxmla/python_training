901b2dd446ac1ba046a0221f73625d229b743542
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2019-10-30 06:45:32 +0100

    test_modify_some_group_name po id vs DB

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,16 @@
 __author__ = 'mla'
 from model.group import Group
-from random import randrange
+import random
+import string
+
+def random_string(prefix, maxlen):
+   symbols = string.ascii_letters + string.digits + " "*10 # string.punctuation +
+   return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]).strip()
+
+#def random_string(maxlen):
+#    symbols = string.ascii_letters + string.digits + " " * 10
+#    chars = "".join([random.choice(symbols) for i in range(random.randrange(5,maxlen))]).strip()
+#    return chars
 
 
 def test_modify_first_group_name(app, db):
@@ -16,19 +26,17 @@ def test_modify_first_group_name(app, db):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     #app.session.logout()
 
-def test_modify_some_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify2_group_by_index(index, group)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+def test_modify_some_group_name(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20)))
+    old_groups = db.get_group_list()
+    chosen_group = random.choice(old_groups)
+    app.group.modify2_group_by_id(chosen_group.id, Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20)))
+    new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
     #app.session.logout()
+    if check_ui:
+        assert len(old_groups) == len(app.group.get_group_list())
 
 #def test_modify_group_header(app):
 #    if app.group.count() == 0:
